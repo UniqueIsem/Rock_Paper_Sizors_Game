595e14413c8a3bf5f6b8ffdbdebf9aa18ac5db2f
Isaac Santos <isaac.emanuel@live.com>
2024-03-18 20:47:29 -0600

    uploading code

---
+++ rock_paper_sizors.py
@@ -0,0 +1,16 @@
+import random
+
+def main():
+    opciones = ['rock', 'paper', 'sizors']
+
+    welcomeMessage = """
+    OPCIONES DEL JUEGO
+    1. ROCK
+    2. PAPER
+    3. SIZORS
+    """
+    print(welcomeMessage)
+    
+
+if __name__ == '__main__':
+    main()
